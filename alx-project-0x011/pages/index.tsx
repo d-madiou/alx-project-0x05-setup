f030699c2dfc3b639b2a1489f7739071aa0378bb
d-madiou <madioudiallo846@gmail.com>
2025-06-23 02:48:55 +0800

    The first one

--- alx-project-0x011/pages/index.tsx
+++ alx-project-0x011/pages/index.tsx
@@ -1,9 +1,6 @@
-
 import ImageCard from "@/components/common/ImageCard";
 import { ImageProps } from "@/interfaces";
-import { useState } from "react";
-
-
+import React, { useState } from "react";
 
 const Home: React.FC = () => {
   const [prompt, setPrompt] = useState<string>("");
@@ -14,9 +11,28 @@ const Home: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
 
- const handleGenerateImage = async () => {
-    console.log("Generating Image")
-    console.log(process.env.NEXT_PUBLIC_GPT_API_KEY)
+  const handleGenerateImage = async () => {
+    setIsLoading(true);
+    const resp = await fetch('/api/generate-image', {
+      method: 'POST',
+      body: JSON.stringify({
+        prompt
+      }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+
+
+    if (!resp.ok) {
+      setIsLoading(false)
+      return;
+    }
+
+    const data = await resp.json()
+    setIsLoading(false)
+    setImageUrl(data?.message);
+    setGeneratedImages((prev) => [...prev, { imageUrl: data?.message, prompt }])
   };
 
   return (
@@ -39,15 +55,36 @@ const Home: React.FC = () => {
             onClick={handleGenerateImage}
             className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200"
           >
-            {/* {
+            {
               isLoading ? "Loading..." : "Generate Image"
-            } */}
-            Generate Image
+            }
           </button>
         </div>
 
         {imageUrl && <ImageCard action={() => setImageUrl(imageUrl)} imageUrl={imageUrl} prompt={prompt} />}
       </div>
+      {
+        generatedImages.length ? (
+          <div className="">
+            <h3 className="text-xl text-center mb-4">Generated Images</h3>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 border max-w-full md:max-w-[1100px] p-2 overflow-y-scroll h-96">
+              {generatedImages?.map(
+                ({ imageUrl, prompt }: ImageProps, index) => (
+                  <ImageCard
+                    action={() => setImageUrl(imageUrl)}
+                    imageUrl={imageUrl}
+                    prompt={prompt}
+                    key={index}
+                    width="w-full"
+                    height="h-40"
+                  />
+                )
+              )}
+            </div>
+          </div>
+
+        ) : ""
+      }
     </div>
   );
 };
